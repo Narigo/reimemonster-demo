0f06736787d7e9abc1fb1659b174bc553868d319
Joern Bernhardt <joern.bernhardt@compose.us>
2022-03-06 00:06:05 +0100

    feat: Move save button into own component
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- src/lib/PoemCreator/PoemCreator.svelte
+++ src/lib/PoemCreator/PoemCreator.svelte
@@ -5,6 +5,7 @@
 	import { lastWordTyped } from '$lib/store/poem.store';
 	import RhymeSuggestion from '$lib/RhymeSuggestion/RhymeSuggestion.svelte';
 	import PoemInput from './PoemInput/PoemInput.svelte';
+	import SavePoemButton from './SavePoemButton/SavePoemButton.svelte';
 </script>
 
 <section>
@@ -13,7 +14,7 @@
 			<PoemDropdown>
 				<PoemDropdownList />
 			</PoemDropdown>
-			<button type="button"><span>+</span></button>
+			<SavePoemButton />
 		</div>
 	</PoemTitle>
 	<PoemInput />
@@ -33,25 +34,5 @@
 		display: flex;
 		flex: 0;
 		justify-content: center;
-	}
-
-	button {
-		align-items: center;
-		background-color: var(--background-color);
-		border: 1px solid rgba(0, 0, 0, 0.2);
-		display: flex;
-		font: var(--poem-title-font);
-		justify-content: center;
-		margin: 0;
-		padding: 0;
-		height: 2em;
-		width: 2em;
-	}
-	button:focus {
-		border: 2px solid #000;
-		outline: none;
-	}
-	span {
-		width: 2em;
 	}
 </style>
---
+++ src/lib/PoemCreator/SavePoemButton/SavePoemButton.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import { poem } from '$lib/store/poem.store';
+
+	function savePoem() {
+		console.log('saving as new poem', $poem);
+	}
+</script>
+
+<button type="button" on:click={savePoem}><span>+</span></button>
+
+<style>
+	button {
+		align-items: center;
+		background-color: var(--background-color);
+		border: 1px solid rgba(0, 0, 0, 0.2);
+		display: flex;
+		font: var(--poem-title-font);
+		justify-content: center;
+		margin: 0;
+		padding: 0;
+		height: 2em;
+		width: 2em;
+	}
+	button:focus {
+		border: 2px solid #000;
+		outline: none;
+	}
+	span {
+		width: 2em;
+	}
+</style>
